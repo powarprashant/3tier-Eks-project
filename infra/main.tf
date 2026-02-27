31be3962ca496d24acfb7044c023d4d00fa8e50b
powarprashant <prashantkpowar24@gmail.com>
2026-02-27 15:07:35 +0530

    updated providers

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,26 @@
 module "primary" {
   source = "./regions/primary"
+
+  providers = {
+    aws = aws.primary
+  }
 }
 
 module "secondary" {
   source = "./regions/secondary"
+
+  providers = {
+    aws = aws.secondary
+  }
 }
 
 module "aurora" {
   source = "./modules/aurora"
+
+  providers = {
+    aws.primary   = aws.primary
+    aws.secondary = aws.secondary
+  }
 
   db_password = var.db_password
 }
