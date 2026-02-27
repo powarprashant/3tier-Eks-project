3491ed1ba6d149242293764f89dba1bf8efb7b1b
powarprashant <prashantkpowar24@gmail.com>
2026-02-27 15:18:06 +0530

    updated providers

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"   # FIX: Pin provider to v5, avoid v6 breaking changes
+      version = ">= 5.40.0, < 6.0.0"
     }
   }
 }
@@ -15,19 +15,16 @@ variable "secondary_region" {
   default = "ap-south-1"
 }
 
-# Primary provider (used for primary EKS & Aurora writer)
 provider "aws" {
   alias  = "primary"
   region = var.primary_region
 }
 
-# Secondary provider (used for secondary EKS & Aurora reader)
 provider "aws" {
   alias  = "secondary"
   region = var.secondary_region
 }
 
-# Default provider (optional, used when a provider alias is not explicitly passed)
 provider "aws" {
   region = var.primary_region
 }
