85cc90685d453c5ec1bf4d75dce1dede085b115e
powarprashant <prashantkpowar24@gmail.com>
2026-03-02 17:04:08 +0530

    updated version.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,11 +1,9 @@
 terraform {
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.40"
+      source = "hashicorp/aws"
     }
   }
 }
-
