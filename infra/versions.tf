4e4b39607e11edca3d57f803fcbe2251fc752392
powarprashant <prashantkpowar24@gmail.com>
2026-03-02 16:50:05 +0530

    removed version file

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.4.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.40"   
+      version = "~> 6.40"
     }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
